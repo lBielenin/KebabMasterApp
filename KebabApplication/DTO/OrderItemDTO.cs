24d250161441a31dddd31b0500311315722bc822
Lucas <lukasz.bielenin@microsoft.wsei.edu.pl>
2022-07-06 19:36:47 +0200

    Move dtos to application layer

---
+++ KebabApplication/DTO/OrderItemDTO.cs
@@ -0,0 +1,16 @@
+﻿using KebabCore.DomainModels.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KebabApplication.DTO
+{
+    public class OrderItemDTO
+    {
+        public MenuItem MenuItem { get; set; }
+
+        public byte Quantity { get; set; } = 1;
+    }
+}
